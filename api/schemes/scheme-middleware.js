191107cf3e5fc67e6123a4f6b61d001e94e6dcba
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-08 18:10:41 -0500

    progress

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require("../../data/db-config");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +7,17 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
-}
+const checkSchemeId = async (req, res, next) => {
+  const scheme = await db("schemes").where("scheme_id", req.params.id).first();
+  if (scheme) {
+    next();
+  } else {
+    next({
+      status: 404,
+      message: `scheme with scheme_id ${req.params.id} not found`,
+    });
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -18,9 +27,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +38,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
