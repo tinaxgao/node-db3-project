48a0295987220c3e069fb5238cd1ebefb447f73f
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-09 01:10:14 -0500

    sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,13 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select categoryname, productname
+from product as p
+join category as c
+on p.categoryid = c.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
